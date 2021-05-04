b664cb55023ce315da194fd246da1afdf5b17842
tnobile <takao.nosaka@outlook.com>
2021-05-04 23:59:41 +0200

    list

--- src/App.js
+++ src/App.js
@@ -52,7 +52,19 @@ export default function App() {
   );
 
   function About() {
-    return <h2>About</h2>;
+    return (
+      <>
+        <h2 className="text-center">About</h2>
+        <div>
+          several approach to fetch data via GraphQL
+        <ul>
+            <li>apollo</li>
+            <li>fetch</li>
+            <li>axiom</li>
+          </ul>
+        </div>
+      </>
+    )
   }
 
   function Apollo() {
